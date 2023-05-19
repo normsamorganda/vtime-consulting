31a4e7e844436c0ebcc9754973e80c776803ce90
Norman <norman.web.ph@tripleonetech.net>
2023-05-19 13:30:32 +0800

    about-us page

--- src/views/ServiceContainer/ServiceContainer.tsx
+++ src/views/ServiceContainer/ServiceContainer.tsx
@@ -12,12 +12,12 @@ const ServiceContainer = () => {
         <Text
           size='normal'
           className={cx('text-black mt-5 leading-6 text-center', 'tablet:!text-sm', 'phone:!text-[12px]')}>
-        Being a one-stop service shop, we offer a wide range of services, from a company's incorporation to populating it to actual operations and physical locations.
+        Being a one-stop service shop, we offer a wide range of services, from a company&apos;ss incorporation to populating it to actual operations and physical locations.
         </Text>
         <Text
           size='normal'
           className={cx('text-black mt-5 leading-6 font-bold text-center', 'tablet:!text-sm', 'phone:!text-[12px]')}>
-            We offer a wide range of services that span from a company's incorporation to actual operations.
+            We offer a wide range of services that span from a company&apos;ss incorporation to actual operations.
         </Text>
     </div>
   )
