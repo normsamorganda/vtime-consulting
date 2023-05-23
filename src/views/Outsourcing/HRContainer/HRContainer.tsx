9eb0d9aaf465433d6cbd798ac91df96bf7d181c2
Norman <norman.web.ph@tripleonetech.net>
2023-05-23 13:45:47 +0800

    title fix

--- src/views/Outsourcing/HRContainer/HRContainer.tsx
+++ src/views/Outsourcing/HRContainer/HRContainer.tsx
@@ -6,7 +6,7 @@ const HRContainer = () => {
   return (
     <div className='w-4/5 md:w-3/5 mx-auto '>
             <h1 className='font-bold text-center text-lg md:text-left'>Human Resources</h1>
-            <p className='text-[12px] text-justify pt-3 md:text-base'>Our diverse team of experts all work together to ensure that you company's HR requirements and infrastructure is build constant grwoth and effciency</p>
+            <p className='text-[12px] text-justify pt-3 md:text-base'>Our diverse team of experts all work together to ensure that you company &apos; HR requirements and infrastructure is build constant grwoth and effciency</p>
             <div className='flex flex-col justify-around items-start mt-10 lg:flex-row'>
                 <div className='shadow-lg shadow-gray-500/50 rounded-xl order-2 lg:order-1 md:w-[500px]'>
                     <div className='pt-6 pl-12 text-justify'>
@@ -18,7 +18,7 @@ const HRContainer = () => {
                         </ul>
                         <ul className='text-[12px] pb-6'>
                             <li className='font-bold list-disc md:text-lg'>Record Management</li>
-                            <li className='px-5 pt-3 md:text-sm'>Supervise and administer your company's digital or paper records in any format</li>
+                            <li className='px-5 pt-3 md:text-sm'>Supervise and administer your company&apos; digital or paper records in any format</li>
                         </ul>
                         <ul className='text-[12px] pb-6'>
                             <li className='font-bold list-disc md:text-lg  '>Employee Benefits</li>
