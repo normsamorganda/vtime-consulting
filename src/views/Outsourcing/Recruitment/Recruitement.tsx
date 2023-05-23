67f8faa9cd77904f59a6b8d6aa9e2ee8f2d562ce
Norman <norman.web.ph@tripleonetech.net>
2023-05-23 13:54:11 +0800

    title fix

--- src/views/Outsourcing/Recruitment/Recruitement.tsx
+++ src/views/Outsourcing/Recruitment/Recruitement.tsx
@@ -8,17 +8,17 @@ import cx from "clsx"
 const Recruitement = () => {
 
   let recruitements = [
-    {
+    { id:1,
       title:"Manpower Service",
       description:"BAC sought to distinguish itself as a reliable partner of foreign and loca employers in providing a labor force; a solid believer in quality servicesl and a result-oriented enterprice that derives satisfaction from meeting the needs of its customers without compromising its highest ervice and ethical standards" , 
       image: manPower,
     },
-    {
+    { id:2,
       title:"Expat Staff Deployment",
       description:"International applicants require a unique recruitment service. Having assisted several internal firms, we understand international cultues and the business requirements needed to recruit and deploy internal candidates for local emploment in the Philippines" , 
       image: team,
     },
-    {
+    { id:3,
       title:"Local Staff Deployment",
       description:"When you are seeking local staff in the Philippines, we will provide you with a fast and cost-effective recruitment/deployment solution that will take the time, cost and hassle of looking for local talent here in the Philippines" , 
       image: remoteAssistance,
@@ -39,7 +39,7 @@ const Recruitement = () => {
           </p>
         </div>
         <div className={cx('flex justify-between gap-0 items-center', 'tablet:flex-col gap-16', 'phone:gap-5')}>
-    {recruitements.map((recruite) => (<AdvantageBox title={recruite.title} description={recruite.description} image={recruite.image}/>)
+    {recruitements.map((recruite) => (<AdvantageBox key={recruite.id} title={recruite.title} description={recruite.description} image={recruite.image}/>)
     )}
         </div>
       </SectionContainer>
