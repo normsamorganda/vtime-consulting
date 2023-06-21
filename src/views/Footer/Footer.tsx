314cc559886132ddfd163a440a845565e5810aac
Norman <norman.web.ph@tripleonetech.net>
2023-06-21 15:12:30 +0800

    navbar

--- src/views/Footer/Footer.tsx
+++ src/views/Footer/Footer.tsx
@@ -16,22 +16,18 @@ import {
 
 const Footer = () => {
   return (
-    <SectionContainer>
+    <div className="max-w-[57rem] mx-auto tablet:mx-3">
       {/* left section */}
-      <div className={cx(' grid grid-cols-3 grid-rows-3', 'tablet:!grid-rows-5 tablet:grid-cols-1 tablet:!justify-start')}>
+      <div className='grid grid-cols-2 grid-rows-2 gap-y-6 mb-5 tablet:gap-3'>
         <article className={cx("row-start-1", 'tablet:!items-center tablet:col-span-2')}>
           <Image
             src={blackLogo}
             alt='logo'
-            className="w-full h-[70px]"
+            className="max-w-[90%] h-[70px] tablet:mx-auto"
           />
         </article>
-        {/* <Text
-          size='description'
-          className={cx('font-light  row-start-2 ', 'tablet:col-span-2 tablet:!max-w-[90%]')}>
-          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Saepe porro magnam aspernatur tempora culpa eum, earum molestiae cumque quisquam ipsam!
-        </Text> */}
-        <article className={cx('flex items-center row-start-2 row-span-3 col-start-3 mt-5', 'tablet:!row-start-4 tablet:flex-wrap')}>
+        {/* <article className='flex items-center justify-center row-start-2 row-span-3 col-start-2 mt-5 tablet:row-start-4 tablet:flex-wrap'> */}
+        <article className="flex justify-between items-center tablet:my-2 tablet:col-span-2">
           <Text
             size='tiny'
             className='!font-semibold hover:underline'>
@@ -65,80 +61,40 @@ const Footer = () => {
           </Text>
         </article>
 
-    <div className="grid grid-cols-2 text-xs lg:row-start-2 lg:row-span-3 lg:justify-end lg:text-left lg:items-center lg:mt-5">
+    <div className="grid grid-cols-2 text-xs row-start-2 tablet:col-span-2 tablet:my-2 tablet:gap-y-3">
           <h1 className="col-span-2 font-bold text-left"><span>Contact</span></h1>
-          <div>
-            <div>
-              Address:
-            </div>
-          </div>
-          <div>
-          23A Chatham House,  VA Rufino Street,  Salcedo Village Makati City, Manila Philippines
-          </div>
-          <div>
-          Office:
-          </div>
-          <div>
-          +63 2625-0632 / +632831-9272
-          </div>
           <div>
           Mobile:
           </div>
           <div>
+          +63 2625-0632 / +632831-9272
+          </div>
+          <div className="row-start-3 col-start-2">
           +63 9178002200 (English,中文)
           </div>
-          <div>
+          <div className="row-start-4">
           Email:
           </div>
-          <div>
+          <div className="col-start-2">
           sales@fhi365.com
           </div>
       </div>
- 
-         {/* <div className="row-start-1 col-start-2 row-span-3 flex flex-col justify-end items-center">
-          <div className="grid grid-cols-2 text-sm justify-center text-center">
-          <h1 className="col-span-2 font-bold text-left"><span className="md:ml-[71px]">Contact</span></h1>
-          <div>
-            Address:
+        <div className={cx('row-start-1 col-start-2 flex justify-center items-center gap-10 ', 'tablet:!row-start-3 tablet:!col-start-1 tablet:!justify-start tablet:col-span-2 tablet:my-2')} >
+          <div className="w-full text-left">
+              <p className="uppercase">Connect With Us</p>
+              <div className="flex mt-2">
+                <FaFacebookF className='grayscale-1 text-gray-300 h-5 w-5 mx-3' />
+                <FaTwitter className='grayscale-1 text-gray-300 h-5 w-5 mx-3' />
+                <FaLinkedinIn className='grayscale-1 text-gray-300 h-5 w-5 mx-3' />
+              </div>
+             
           </div>
-          <div>
-          23A Chatham House,  VA Rufino Street,  Salcedo Village Makati City, Manila Philippines
-          </div>
-          <div>
-          Office:
-          </div>
-          <div>
-          +63 2625-0632 / +632831-9272
-          </div>
-          <div>
-          Mobile:
-          </div>
-          <div>
-          +63 9178002200 (English,中文)
-          </div>
-          <div>
-          Email:
-          </div>
-          <div>
-          sales@fhi365.com
-          </div>
-        </div>
-        </div>  */}
-      
-        <div className={cx('row-start-1 col-start-3 flex justify-start gap-10 items-center ', 'tablet:!row-start-5 tablet:!col-start-1 tablet:!justify-start tablet:col-span-2')} >
-          <p className="uppercase">Connect With Us</p>
-          <FaFacebookF className='grayscale-1 text-gray-300 h-5 w-5' />
-          <FaTwitter className='grayscale-1 text-gray-300 h-5 w-5' />
-          <FaLinkedinIn className='grayscale-1 text-gray-300 h-5 w-5' />
         </div >
       </div >
 
       {/* right section */}
-
-    </SectionContainer >
+      </div>
   )
 }
-function span() {
-  return <span className='px-3'>/</span>
-}
+
 export default Footer
